317c33a4145e5566f355822f42f11826a811354c
Anna Garay <65153386+spearhead-ag@users.noreply.github.com>
2020-06-01 02:33:19 -0400

    Add files via upload

--- second.sh
+++ second.sh
@@ -1,4 +1,11 @@
+# ============================================== #
+# This script runs a script with given argument  #
+# ============================================== #
 #!/bin/bash
+
 echo "Hi there!"
+
+#prints the argument when running the script
 echo "It's good to see you $1."
+
 exit 0
